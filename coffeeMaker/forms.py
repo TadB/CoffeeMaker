c7dbd927cb4bea4b80baf2c7e7bc73fc01a7cd42
acny <tadeusz.brzytwa@gmail.com>
2018-12-19 17:07:34 +0100

    call make_coffee() on button click

--- coffeeMaker/forms.py
+++ coffeeMaker/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField
 
+
 class CoffeeTypesForm(FlaskForm):
-    submit = SubmitField('Caffè latte')
+    submit = SubmitField('Espresso')
--- coffeeMaker/routes.py
+++ coffeeMaker/routes.py
@@ -1,8 +1,21 @@
+from flask import render_template, url_for, redirect
 from coffeeMaker import app
-from flask import render_template, flash, redirect
 from coffeeMaker.forms import CoffeeTypesForm
+from coffeeMaker.utils import make_coffee
 
-@app.route("/", methods=['GET', 'POST'])
+
+@app.route("/home")
+@app.route("/")
 def home():
+    return render_template('home.html')
+
+
+@app.route("/choose", methods=['GET', 'POST'])
+def main_panel():
     form = CoffeeTypesForm()
-    return render_template('choose_coffee.html', title='Select delicious coffee', form=form)
+    if form.validate_on_submit():
+        input = str(form.submit.label.text)
+        make_coffee(input)
+        return redirect(url_for('home'))
+    return render_template(
+        'choose_coffee.html', title='Select delicious coffee', form=form)
